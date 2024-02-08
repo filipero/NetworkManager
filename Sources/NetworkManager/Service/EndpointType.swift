11d11850135d5f59ebefecbfc6694be0e1370819
Filipe Oliveira <Filipe.ro.ol@gmail.com>
2024-02-07 23:17:31 -0300

    remove base endpoint

--- Sources/NetworkManager/Service/EndpointType.swift
+++ Sources/NetworkManager/Service/EndpointType.swift
@@ -18,20 +18,3 @@ public protocol EndpointType {
     var task: HTTPTask { get }
     var headers: HTTPHeaders? { get }
 }
-
-extension EndpointType {
-    public var baseURL: URL {
-        guard let url = URL(string: environmentBaseURL) else { fatalError("baseURL could not be configured.") }
-        return url
-    }
-    
-    var environmentBaseURL: String {
-        switch environment {
-        case .production: return "https://rickandmortyapi.com/api"
-        }
-    }
-    
-    public var environment: NetworkEnvironment {
-        .production
-    }
-}
